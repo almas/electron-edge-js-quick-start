d01bc1e78db7130f07ed5eea275341b5844e6265
Malte Klapper <malte.klapper@posteo.de>
2021-03-05 16:40:34 +0100

    changed target framework to .NET5
    - changed target framework of QuickStart to net5.0
    - referenced latest electron-edge-js from ArticySoftware

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 1. Install dependencies `npm install` or `yarn`
 2. Build electron-edge-js via `npm run rebuild:debug` or `yarn rebuild:debug` // or `:release`
 3. Build `src\QuickStart.sln` using Visual Studio 2019 or JetBrains Rider or run `dotnet build src/QuickStart.sln`
-4. To run the app using .NET Core use `npm start` or `npm run start:core`
-5. ~~To run the app using .NET Standard use `npm run start:standard`~~
+4. To run the app using .NET 5 use `npm start` or `npm run start:core`
--- renderer.js
+++ renderer.js
@@ -3,9 +3,10 @@ var version = location.search.split('version=')[1];
 var namespace = 'QuickStart.' + version.charAt(0).toUpperCase() + version.substr(1);
 if(version === 'core') version = 'coreapp';
 
-const baseNetAppPath = path.join(__dirname, '/src/'+ namespace +'/bin/Debug/net'+ version +'2.0');
+const baseNetAppPath = path.join(__dirname, '/src/'+ namespace +'/bin/Debug/net5.0');
 
 process.env.EDGE_USE_CORECLR = 1;
+
 if(version !== 'standard')
     process.env.EDGE_APP_ROOT = baseNetAppPath;
 
